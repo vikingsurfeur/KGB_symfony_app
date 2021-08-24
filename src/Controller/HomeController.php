4d66378affb5076cbf3c2af704fe3ad3d31c2a00
David Bouscarle <d.bouscarle@gmail.com>
2021-08-24 10:33:13 +0200

    Implement Paginator KNP, start modify Home Controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\MissionRepository;
+use Knp\Component\Pager\PaginatorInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(MissionRepository $missionRepository): Response
+    public function index(
+        MissionRepository $missionRepository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',

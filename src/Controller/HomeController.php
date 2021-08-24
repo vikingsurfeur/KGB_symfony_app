256b74217c02be41652a31f3d12ce672c35b7cac
David Bouscarle <d.bouscarle@gmail.com>
2021-08-24 10:46:58 +0200

    Implement pagination on home page, works fine

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -18,9 +18,12 @@ class HomeController extends AbstractController
         Request $request
     ): Response
     {
+        $data = $missionRepository->findAll();
+        $missions = $paginator->paginate($data, $request->query->getInt('page', 1), 3);
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'missions' => $missionRepository->findAll(),
+            'missions' => $missions,
         ]);
     }
 }

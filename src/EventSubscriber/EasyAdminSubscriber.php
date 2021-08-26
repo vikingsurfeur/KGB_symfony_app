48d15bccbf1d4b24066fba65483255cfcf4189b1
David Bouscarle <d.bouscarle@gmail.com>
2021-08-26 08:51:42 +0200

    Add the EventSubscriber to the admin id to the mission create, works fine

---
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\Mission;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Security;
+
+class EasyAdminSubscriber implements EventSubscriberInterface
+{
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            BeforeEntityPersistedEvent::class => ['setUserId'],
+        ];
+    }
+
+    public function setUserId(BeforeEntityPersistedEvent $event): void
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!$entity instanceof Mission) {
+            return;
+        }
+
+        $user = $this->security->getUser();
+        $entity->setUser($user);
+    }
+}

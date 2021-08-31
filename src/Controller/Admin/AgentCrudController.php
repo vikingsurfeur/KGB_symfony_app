f0169b100d24472cd22b3b40aa0e49ce1584757b
David Bouscarle <d.bouscarle@gmail.com>
2021-08-31 11:07:25 +0200

    Work on Event Subscriber and error page

--- src/Controller/Admin/AgentCrudController.php
+++ src/Controller/Admin/AgentCrudController.php
@@ -8,7 +8,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CountryField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class AgentCrudController extends AbstractCrudController
--- src/EventSubscriber/EasyAdminSubscriber.php
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -3,9 +3,13 @@
 namespace App\EventSubscriber;
 
 use App\Entity\Mission;
+use App\Entity\Agent;
+use App\Entity\Contact;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Core\Security;
+
 
 class EasyAdminSubscriber implements EventSubscriberInterface
 {
@@ -21,6 +25,12 @@ class EasyAdminSubscriber implements EventSubscriberInterface
         return [
             BeforeEntityPersistedEvent::class => [
                 ['setUserId'],
+                ['checkPersistAgentNationality'],
+                ['checkPersistContactNationality'],
+            ],
+            BeforeEntityUpdatedEvent::class => [
+                ['checkUpdateAgentNationality'],
+                ['checkUpdateContactNationality'],
             ],
         ];
     }
@@ -35,5 +45,66 @@ class EasyAdminSubscriber implements EventSubscriberInterface
 
         $user = $this->security->getUser();
         $entity->setUser($user);
+    }
+
+    public function checkPersistAgentNationality(BeforeEntityPersistedEvent $event)
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!$entity instanceof Agent) {
+            return;
+        }
+
+        $agentNationality = $entity->getNationality();
+        // select all the nationality of the targets in the mission entity
+        $targetNationalities = $entity->getMission()->getTargets()->map(function($nationalities){
+            return $nationalities->getNationality();
+        });
+
+        foreach ($targetNationalities as $targetNationality)
+        {
+            if ($targetNationality === $agentNationality) {
+                throw new \Exception('Agent and Target have the same nationality, it\'s not possible');
+            }
+        }
+    }
+
+    public function checkUpdateAgentNationality(BeforeEntityUpdatedEvent $event): void
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!$entity instanceof Agent) {
+            return;
+        }
+
+        $agentNationality = $entity->getNationality();
+        // select all the nationality of the targets in the mission entity
+        $targetNationalities = $entity->getMission()->getTargets()->map(function($nationalities){
+            return $nationalities->getNationality();
+        });
+
+        foreach ($targetNationalities as $targetNationality)
+        {
+            if ($targetNationality === $agentNationality) {
+                throw new \Exception('Agent and Target have the same nationality, it\'s not possible');
+            }
+        }
+    }
+
+    public function checkPersistContactNationality(BeforeEntityPersistedEvent $event)
+    {
+        $entity = $event->getEntityInstance();
+
+        if (!$entity instanceof Contact) {
+            return;
+        }
+
+        $contactNationality = $entity->getNationality();
+        // select all the nationality of the targets in the mission entity
+        $missionCountry = $entity->getMission()->getCountry();
+
+        if ($missionCountry !== $contactNationality) {
+            throw new \Exception('Contact must come from the mission country');
+        }
     }
 }
